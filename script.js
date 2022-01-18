9c7f4ca769ae87a1da10506c7a75d7a9ea5a9949
unknown <webcatdev@gmail.com>
2022-01-18 13:41:03 +0300

    trying git commands

---
+++ script.js
@@ -0,0 +1,37 @@
+const genislet = document.getElementById("genislet");
+const oynat = document.getElementById("oynatma-butonu");
+const ayarlar = document.getElementById("ayarlar");
+const inputs = document.getElementsByClassName("ayar");
+const selectElements = document.getElementsByClassName("select-element");
+const rootElement = document.querySelector("html");
+rootElement.style.setProperty("--animation-play-state", "paused");
+
+genislet.addEventListener("click", () => {
+  ayarlar.classList.toggle("genislet");
+  genislet.classList.toggle("genislet");
+});
+
+oynat.addEventListener("click", () => {
+  if (
+    rootElement.style.getPropertyValue("--animation-play-state") === "paused"
+  ) {
+    rootElement.style.setProperty("--animation-play-state", "running");
+  } else {
+    rootElement.style.setProperty("--animation-play-state", "paused");
+  }
+  oynat.classList.toggle("oynat");
+});
+
+for (const input of inputs) {
+  input.addEventListener("change", (e) => {
+    rootElement.style.setProperty(`${e.target.id}`, `${e.target.value}s`);
+  });
+}
+
+for (const selectElement of selectElements) {
+  selectElement.addEventListener("change", (e) => {
+    let selectElementValue =
+      selectElement.options[selectElement.selectedIndex].value;
+    rootElement.style.setProperty(`${e.target.id}`, selectElementValue);
+  });
+}
